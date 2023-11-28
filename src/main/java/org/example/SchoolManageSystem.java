055a44daed8f8d4f023541604ade9e760da0d689
caokuan <845146065@qq.com>
2023-11-27 23:19:25 -0500

    SchoolManageSystem calss finished.

--- src/main/java/org/example/SchoolManageSystem.java
+++ src/main/java/org/example/SchoolManageSystem.java
@@ -19,7 +19,7 @@ public class SchoolManageSystem {
             Student newStudent = new Student(firstName, lastName, department);
             // Add logic to add the new student to the students array
         } else {
-            System.out.println("Department not found.");
+            System.out.println("Can not find Department");
         }
     }
 
@@ -31,7 +31,7 @@ public class SchoolManageSystem {
             Teacher newTeacher = new Teacher(firstName, lastName, departmentId);
             // Add logic to add the new teacher to the teachers array
         } else {
-            System.out.println("Department not found.");
+            System.out.println("Can not find Department");
         }
     }
 
@@ -43,11 +43,116 @@ public class SchoolManageSystem {
             Course newCourse = new Course(courseName, credit, department);
             // Add logic to add the new course to the courses array
         } else {
-            System.out.println("Can not find Departement");
+            System.out.println("Can not find Department");
         }
     }
 
     private Departement searchDepartement(String departmentId) {
         return null;
+    }
+
+    class SchoolManagementSystem {
+        // Existing attributes and methods
+
+        // Method to display all departments that are not null
+        public void displayDepartments() {
+            for (Departement department : departements) {
+                if (department != null) {
+                    System.out.println(department); // Implement Department's toString method
+                }
+            }
+        }
+
+        // Method to display all students with their courses and department information if available
+        public void displayStudents() {
+            for (Student student : students) {
+                if (student != null) {
+                    System.out.println(student); // Implement Student's toString method
+                }
+            }
+        }
+
+        // Method to display all teachers with their courses and department information if available
+        public void displayTeachers() {
+            for (Teacher teacher : teachers) {
+                if (teacher != null) {
+                    System.out.println(teacher); // Implement Teacher's toString method
+                }
+            }
+        }
+
+        // Method to display all courses with their teacher, students, and department information if available
+        public void displayCourses() {
+            for (Course course : courses) {
+                if (course != null) {
+                    System.out.println(course); // Implement Course's toString method
+                }
+            }
+        }
+
+        // Method to search for a department based on departmentId
+        public Departement searchDepartment(String departmentId) {
+            for (Departement department : departements) {
+                if (department != null && department.getDepartmentId().equals(departmentId)) {
+                    return department;
+                }
+            }
+            return null;
+        }
+
+        // Method to search for a teacher based on teacherId
+        public Teacher searchTeacher(String teacherId) {
+            for (Teacher teacher : teachers) {
+                if (teacher != null && teacher.getTeacherId().equals(teacherId)) {
+                    return teacher;
+                }
+            }
+            return null;
+        }
+
+        // Method to search for a course based on courseId
+        public Course searchCourse(String courseId) {
+            for (Course course : courses) {
+                if (course != null && course.getCourseId().equals(courseId)) {
+                    return course;
+                }
+            }
+            return null;
+        }
+
+        // Method to search for a student based on studentId
+        public Student searchStudent(String studentId) {
+            for (Student student : students) {
+                if (student != null && student.getStudentId().equals(studentId)) {
+                    return student;
+                }
+            }
+            return null;
+        }
+
+        // Method to assign a teacher to a specific course
+        public void assignTeacherToCourse(String teacherId, String courseId) {
+            Teacher teacher = searchTeacher(teacherId);
+            Course course = searchCourse(courseId);
+
+            if (teacher != null && course != null) {
+                course.setTeacher(teacher);
+            } else {
+                System.out.println("Teacher or course cannot find.");
+            }
+        }
+
+        // Method to register a course for a student
+        public void registerCourseForStudent(String studentId, String courseId) {
+            Student student = searchStudent(studentId);
+            Course course = searchCourse(courseId);
+
+            if (student != null && course != null) {
+                // Add logic to handle the registration process
+            } else {
+                System.out.println("Student or course cannot find.");
+            }
+            // Additional logic for registration based on the given constraints
+        }
     }
 }
